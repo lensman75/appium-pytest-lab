1eb49c017b6c0f6b18602f58fefcc52ea4ecd4f0
Lensman75 <anministry.mgmt@gmail.com>
2025-04-03 14:30:15 +0300

    Api initial commit

--- api/main.py
+++ api/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, HTTPException
-from fastapi.responses import PlainTextResponse
+from fastapi.responses import PlainTextResponse, HTMLResponse
 
 app = FastAPI()
+status_message="<h1>Done</h1>"
 
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def read_root():
     return {"Test message"}
 
@@ -19,3 +20,12 @@ def read_file():
         raise HTTPException(status_code=404, detail="File not found")
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error: {e}")
+
+@app.get("/executing", response_class=HTMLResponse)
+def sometest():
+    return "<h1>Executing</h1>"
+
+@app.get("/status", response_class=HTMLResponse)
+def stat():
+    global status_message
+    return f"{status_message}"
